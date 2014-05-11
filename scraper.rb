247a734dbbbd0a44386805c039a646ace2622684
Jose Damian Garrido <damian.garrido.munoz@gmail.com>
2014-05-11 14:51:13 -0400

    add few info

--- scraper.rb
+++ scraper.rb
@@ -14,7 +14,10 @@ class Diputados
     profile = profile_page.at('#datos_contacto #votos').search('li')
     profile_img = profile_page.at('#contenido > article > img')[:src].gsub('./manager/.','http://congreso.gob.gt/manager')
     profile_extra_data = profile_page.at('#contenido > article')
-    
+    commision_1 = profile_page.at('.acordeon > li:nth-child(3) > section:nth-child(2) > table:nth-child(1) > tr:nth-child(2) > td:nth-child(2) > p:nth-child(1)').inner_text.strip
+    commision_2 = profile_page.at('.acordeon > li:nth-child(3) > section:nth-child(2) > table:nth-child(1) > tr:nth-child(3) > td:nth-child(2) > p:nth-child(1)').inner_text.strip
+    commision_3 = profile_page.at('.acordeon > li:nth-child(3) > section:nth-child(2) > table:nth-child(1) > tr:nth-child(4) > td:nth-child(2) > p:nth-child(1)').inner_text.strip
+
     record = {
       "uid" => uid,
       "name" => (profile[0].inner_text).gsub('Nombre:','').squeeze(' ').strip,
@@ -25,14 +28,15 @@ class Diputados
       "address" => (profile[3].inner_text).gsub('Dirección de Oficina:','').strip,
       "url" => profile_url,
       "image" => profile_img,
-      "birth_date" => profile_extra_data.search('p')[2].inner_text.gsub('Fecha de Nacimiento: ','')
+      "birth_date" => profile_extra_data.search('p')[2].inner_text.gsub('Fecha de Nacimiento: ',''),
+      "commisions" => commision_1 +' - '+ commision_2 +' - '+ commision_3
     }
 
     #puts '<---------------'
-    #puts record
+    puts record
     #puts '--------------/>'
-    ScraperWiki.save_sqlite(["uid"], record)
-    puts "Adds new record " + record['name']
+    #ScraperWiki.save_sqlite(["uid"], record)
+    #puts "Adds new record " + record['name']
   end
 
   # Obtains the profiles
@@ -49,7 +53,7 @@ class Diputados
       @current_stand = (li.search('td')[3]).inner_text
       url = (li.search('td')[1]).at('a')["href"]
       per_person url, @uid, @party, @current_stand
-      # exit # for fast testing, only the first parliamentarian
+      #exit # for fast testing, only the first parliamentarian
     end
   end
 end
