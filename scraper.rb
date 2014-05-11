cef9513ef8e6ccd3f10ffdd2bb029f7cb604d58b
José Damián Garrido <damian.garrido.munoz@gmail.com>
2014-05-11 11:48:51 -0400

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -14,7 +14,7 @@ class Diputados
     profile = profile_page.at('#datos_contacto #votos').search('li')
     
     puts '+++++++'
-    p profile_img.first.attributes["src"]
+    p profile_img.url
     puts '+++++++'
 
     record = {
